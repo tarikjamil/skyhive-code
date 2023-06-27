2cc7ad4f1a639cc72a73f64f2d01549fa505bdc1
tarikjamil <tarikjamill@gmail.com>
2023-06-27 23:45:08 +0100

    add slider to the homepage

---
+++ page/home.js
@@ -0,0 +1,72 @@
+// slider
+document.addEventListener("DOMContentLoaded", function () {
+  let splide = new Splide(".slider1", {
+    type: "slide",
+    perPage: 4,
+    perMove: 1,
+    autoplay: true,
+    interval: 0,
+    speed: 7000,
+    drag: false,
+    easing: "linear",
+    gap: "24rem",
+    breakpoints: {
+      991: {
+        // Tablet
+        perPage: 1,
+        arrows: false,
+      },
+      767: {
+        // Mobile Landscape
+        perPage: 1,
+        arrows: false,
+      },
+      479: {
+        // Mobile Portrait
+        perPage: 1,
+        arrows: false,
+      },
+    },
+  });
+  splide.mount();
+});
+
+// slider testimonial
+function slider2() {
+  let splides = $(".slider2");
+  for (let i = 0, splideLength = splides.length; i < splideLength; i++) {
+    new Splide(splides[i], {
+      // Desktop on down
+      perPage: 1,
+      perMove: 1,
+      focus: 0, // 0 = left and 'center' = center
+      type: "slide", // 'loop' or 'slide'
+      gap: "24em", // space between slides
+      arrows: "slider", // 'slider' or false
+      pagination: false, // 'slider' or false
+      speed: 600, // transition speed in miliseconds
+      dragAngleThreshold: 30, // default is 30
+      autoWidth: false, // for cards with differing widths
+      rewind: false, // go back to beginning when reach end
+      rewindSpeed: 400,
+      waitForTransition: false,
+      updateOnMove: true,
+      trimSpace: false, // true removes empty space from end of list
+      breakpoints: {
+        991: {
+          // Tablet
+          perPage: 1,
+        },
+        767: {
+          // Mobile Landscape
+          perPage: 1,
+        },
+        479: {
+          // Mobile Portrait
+          perPage: 1,
+        },
+      },
+    }).mount();
+  }
+}
+slider2();
