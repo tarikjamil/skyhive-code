be60f48753a76c6a0616e3d942719d013be5b13d
tarikjamil <tarikjamill@gmail.com>
2023-06-27 23:38:33 +0100

    loading animation

--- script.js
+++ script.js
@@ -26,24 +26,41 @@ gsap.registerPlugin(ScrollTrigger);
 // On Page Load
 function pageLoad() {
   let tl = gsap.timeline();
+
   tl.to(".main-wrapper", {
     opacity: 1,
     ease: "Quint.easeOut",
     duration: 0.5,
   });
-  tl.from("[animation=loading]", {
-    y: "20rem",
-    opacity: "0",
-    stagger: { each: 0.1, from: "start" },
-    ease: "Quint.easeOut",
-    duration: 1,
-  });
-  tl.from("[animation=loading-reverse]", {
-    y: "-20rem",
-    opacity: "0",
-    stagger: { each: 0.1, from: "start" },
-    ease: "Quint.easeOut",
-    duration: 1,
-  });
+
+  // Add a label to mark the starting point of simultaneous animations
+  tl.add("loadingAnimationsStart");
+
+  // Add the 'loading' animation and set its position to the label
+  tl.from(
+    "[animation=loading]",
+    {
+      y: "20rem",
+      opacity: "0",
+      stagger: { each: 0.1, from: "start" },
+      ease: "Quint.easeOut",
+      duration: 1,
+    },
+    "loadingAnimationsStart"
+  ); // <-- position parameter set to the label
+
+  // Add the 'loading-reverse' animation and set its position to the label
+  tl.from(
+    "[animation=loading-reverse]",
+    {
+      y: "-20rem",
+      opacity: "0",
+      stagger: { each: 0.1, from: "start" },
+      ease: "Quint.easeOut",
+      duration: 1,
+    },
+    "loadingAnimationsStart"
+  ); // <-- position parameter set to the label
 }
+
 pageLoad();
