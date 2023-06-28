3c8dae13ce73faf608a7a129752c029664f395f4
tarikjamil <tarikjamill@gmail.com>
2023-06-28 08:26:07 +0100

    Update home.js

--- page/home.js
+++ page/home.js
@@ -26,46 +26,6 @@ document.addEventListener("DOMContentLoaded", function () {
   splide.mount();
 });
 
-// slider testimonial
-function slider2() {
-  let splides = $(".slider2");
-  for (let i = 0, splideLength = splides.length; i < splideLength; i++) {
-    new Splide(splides[i], {
-      // Desktop on down
-      perPage: 1,
-      perMove: 1,
-      focus: 0, // 0 = left and 'center' = center
-      type: "slide", // 'loop' or 'slide'
-      gap: "24em", // space between slides
-      arrows: "slider", // 'slider' or false
-      pagination: false, // 'slider' or false
-      speed: 600, // transition speed in miliseconds
-      dragAngleThreshold: 30, // default is 30
-      autoWidth: false, // for cards with differing widths
-      rewind: false, // go back to beginning when reach end
-      rewindSpeed: 400,
-      waitForTransition: false,
-      updateOnMove: true,
-      trimSpace: false, // true removes empty space from end of list
-      breakpoints: {
-        991: {
-          // Tablet
-          perPage: 1,
-        },
-        767: {
-          // Mobile Landscape
-          perPage: 1,
-        },
-        479: {
-          // Mobile Portrait
-          perPage: 1,
-        },
-      },
-    }).mount();
-  }
-}
-slider2();
-
 document.addEventListener("DOMContentLoaded", function () {
   // Select the text element
   let textElement = document.querySelector(".heading--20.is--successstory");
