f8242e0f41861049b0441fe26fc5c90b1993948f
tarikjamil <tarikjamill@gmail.com>
2023-06-28 08:50:53 +0100

    Update home.js

--- page/home.js
+++ page/home.js
@@ -35,3 +35,24 @@ document.addEventListener("DOMContentLoaded", function () {
   });
   splide.mount();
 });
+
+// slider success stories
+document.addEventListener("DOMContentLoaded", function () {
+  let splide = new Splide(".slider3", {
+    type: "slide",
+    perPage: 1,
+    perMove: 1,
+    gap: "56rem",
+    drag: false,
+    arrows: false,
+    pagination: false,
+    breakpoints: {
+      991: {
+        // Tablet
+        gap: "24rem",
+        drag: true,
+      },
+    },
+  });
+  splide.mount();
+});
