fcbd20f7b7f59afbe757875693d6a90965fac3de
tarikjamil <tarikjamill@gmail.com>
2023-06-28 08:13:02 +0100

    add navbar scroll animation

--- script.js
+++ script.js
@@ -64,3 +64,16 @@ function pageLoad() {
 }
 
 pageLoad();
+
+// navbar color
+$(document).ready(function () {
+  var scrollTop = 0;
+  $(window).scroll(function () {
+    scrollTop = $(window).scrollTop();
+    if (scrollTop >= 100) {
+      $(".navbar").addClass("is--scrolled");
+    } else if (scrollTop < 100) {
+      $(".navbar").removeClass("is--scrolled");
+    }
+  });
+});
