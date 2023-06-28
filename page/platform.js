0116c3f70f9b88cfd1aed154b89f1b59052fc150
tarikjamil <tarikjamill@gmail.com>
2023-06-28 11:15:45 +0100

    platform page

---
+++ page/platform.js
@@ -0,0 +1,37 @@
+// slider
+document.addEventListener("DOMContentLoaded", function () {
+  let splide = new Splide(".slider1", {
+    type: "slide",
+    perPage: 1,
+    perMove: 1,
+    gap: "24rem",
+    breakpoints: {
+      991: {
+        // Tablet
+        arrows: false,
+      },
+    },
+  });
+  splide.mount();
+});
+
+// slider success stories
+document.addEventListener("DOMContentLoaded", function () {
+  let splide = new Splide(".slider2", {
+    type: "slide",
+    perPage: 1,
+    perMove: 1,
+    gap: "56rem",
+    drag: false,
+    arrows: false,
+    pagination: false,
+    breakpoints: {
+      991: {
+        // Tablet
+        gap: "24rem",
+        drag: true,
+      },
+    },
+  });
+  splide.mount();
+});
