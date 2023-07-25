4c9aecc38e0be15e28f15b0433685d131a5eca32
tarikjamil <tarikjamill@gmail.com>
2023-07-25 15:09:30 +0100

    Update entreprise.js

--- page/entreprise.js
+++ page/entreprise.js
@@ -42,24 +42,29 @@ document.addEventListener("DOMContentLoaded", function () {
 
 let imagesAndAnimations = [
   {
+    selector: ".entreprise--hero-wrapper .img--100.is--entreprise",
+    initial: { opacity: 1, scale: 1 },
+    animation: { opacity: 0, scale: 0.6 },
+  },
+  {
     selector: ".entreprise--hero-wrapper .entreprise-hero-img1",
-    initial: { x: "150rem", y: "-20rem", scale: 1.8 },
-    animation: { x: "0rem", y: "0rem", scale: 1 },
+    initial: { x: "150rem", y: "-20rem", opacity: 1, scale: 1.8 },
+    animation: { x: "0rem", y: "0rem", opacity: 1, scale: 1 },
   },
   {
     selector: ".entreprise--hero-wrapper .entreprise-hero-img2",
-    initial: { y: "150rem", scale: 1.15 },
-    animation: { y: "0rem", scale: 1 },
+    initial: { y: "150rem", opacity: 1, scale: 1.15 },
+    animation: { y: "0rem", opacity: 1, scale: 1 },
   },
   {
     selector: ".entreprise--hero-wrapper .entreprise-hero-img3",
-    initial: { x: "-200rem", y: "-50rem", scale: 1.5 },
-    animation: { x: "0rem", y: "0rem", scale: 1 },
+    initial: { x: "-200rem", opacity: 1, y: "-50rem", scale: 1.5 },
+    animation: { x: "0rem", opacity: 1, y: "0rem", scale: 1 },
   },
   {
     selector: ".entreprise--hero-wrapper .entreprise-hero-img4",
-    initial: { y: "-50rem", scale: 1.3 },
-    animation: { y: "0rem", scale: 1 },
+    initial: { y: "-50rem", opacity: 1, scale: 1.3 },
+    animation: { y: "0rem", opacity: 1, scale: 1 },
   },
 ];
 
