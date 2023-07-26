ae79bc45bd6f862842cb19af9997a04bf9ec48b9
tarikjamil <tarikjamill@gmail.com>
2023-07-26 11:58:26 +0100

    edits sliders platform

--- script.js
+++ script.js
@@ -131,3 +131,23 @@ document.addEventListener("DOMContentLoaded", function () {
   });
   splide.mount();
 });
+
+document.addEventListener("DOMContentLoaded", function () {
+  let splide = new Splide(".slider-resources", {
+    type: "slide",
+    perPage: 1,
+    perMove: 1,
+    gap: "24rem",
+    drag: false,
+    arrows: false,
+    pagination: false,
+    breakpoints: {
+      991: {
+        // Tablet
+        gap: "24rem",
+        drag: true,
+      },
+    },
+  });
+  splide.mount();
+});
