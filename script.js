afe22d9bde5060ac48c1a03fb1fe81cb56e496d6
tarikjamil <tarikjamill@gmail.com>
2023-07-26 11:56:48 +0100

    Update script.js

--- script.js
+++ script.js
@@ -90,9 +90,30 @@ $(".navbar--usecases-dropdown").on("click", function () {
   $(".navbar--dropwdown-text-wrapper").toggleClass("is--active");
 });
 
-// slider success stories
+// slider products
 document.addEventListener("DOMContentLoaded", function () {
   let splide = new Splide(".is--slider-products", {
+    type: "slide",
+    perPage: 1,
+    perMove: 1,
+    gap: "24rem",
+    drag: false,
+    arrows: false,
+    pagination: false,
+    breakpoints: {
+      991: {
+        // Tablet
+        gap: "24rem",
+        drag: true,
+      },
+    },
+  });
+  splide.mount();
+});
+
+// slider testimonials
+document.addEventListener("DOMContentLoaded", function () {
+  let splide = new Splide(".is--testimonial-slider", {
     type: "slide",
     perPage: 1,
     perMove: 1,
