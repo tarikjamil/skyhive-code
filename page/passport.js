79e13607f14945132933961db23023413ccdd58d
tarikjamil <tarikjamill@gmail.com>
2023-07-26 11:15:25 +0100

    passport

---
+++ page/passport.js
@@ -0,0 +1,19 @@
+document.addEventListener("DOMContentLoaded", function () {
+  let splide = new Splide(".slider-resources", {
+    type: "slide",
+    perPage: 1,
+    perMove: 1,
+    gap: "24rem",
+    drag: false,
+    arrows: false,
+    pagination: false,
+    breakpoints: {
+      991: {
+        // Tablet
+        gap: "24rem",
+        drag: true,
+      },
+    },
+  });
+  splide.mount();
+});
