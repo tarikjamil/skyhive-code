864162affaa2f9e26dbd4c411a77955a88c5f586
tarikjamil <tarikjamill@gmail.com>
2023-07-27 12:15:05 +0100

    Update hcos.js

--- page/hcos.js
+++ page/hcos.js
@@ -36,35 +36,64 @@ $(".is--hcos--graph-parent").each(function (index) {
   tl.from($(this).find(".img--320"), {
     scale: 0.2,
     opacity: 0,
-    duration: 1,
+    duration: 0.6,
     stagger: { each: 0.1, from: "start" },
   });
   tl.from($(this).find(".img--100.is--rotating"), {
     scale: 0.2,
     opacity: 0,
-    duration: 1,
+    duration: 0.6,
     delay: 0.5,
     stagger: { each: 0.1, from: "start" },
   });
   tl.from($(this).find(".hcos--rotating-text.is--big"), {
     scale: 0.2,
     opacity: 0,
-    duration: 1,
+    duration: 0.6,
     delay: 0.5,
     stagger: { each: 0.1, from: "start" },
   });
   tl.from($(this).find(".hcos--rotating-text.is--small"), {
     scale: 0.2,
     opacity: 0,
-    duration: 1,
+    duration: 0.6,
     delay: 0.5,
     stagger: { each: 0.1, from: "start" },
   });
   tl.from($(this).find(".is--rotating-reverse"), {
     scale: 0.2,
     opacity: 0,
-    duration: 1,
+    duration: 0.6,
     delay: -1,
+    stagger: { each: 0.1, from: "start" },
+  });
+});
+
+$(".hcos--grid-os").each(function (index) {
+  let triggerElement = $(this);
+
+  let tl = gsap.timeline({
+    scrollTrigger: {
+      trigger: triggerElement,
+      start: "top center-=100",
+    },
+  });
+  tl.from($(this).find(".hcos--os-element.is--middle"), {
+    y: "50rem",
+    opacity: 0,
+    duration: 0.6,
+    stagger: { each: 0.1, from: "start" },
+  });
+  tl.from($(this).find(".flex--24gap.is--first").find(".hcos--os-element"), {
+    x: "-50rem",
+    opacity: 0,
+    duration: 0.6,
+    stagger: { each: 0.1, from: "start" },
+  });
+  tl.from($(this).find(".flex--24gap.is--second").find(".hcos--os-element"), {
+    x: "50rem",
+    opacity: 0,
+    duration: 0.6,
     stagger: { each: 0.1, from: "start" },
   });
 });
