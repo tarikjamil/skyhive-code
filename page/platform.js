8493526b8a751838873d3d32d8ae7047d410e442
tarikjamil <tarikjamill@gmail.com>
2023-07-28 11:23:33 +0100

    Update platform.js

--- page/platform.js
+++ page/platform.js
@@ -1,6 +1,5 @@
-$(".platform--graph-parent img").each(function (index) {
+$(".platform--graph-parent").each(function (index) {
   let triggerElement = $(this);
-  let targetElement = $(this);
 
   let tl = gsap.timeline({
     scrollTrigger: {
@@ -8,10 +7,53 @@ $(".platform--graph-parent img").each(function (index) {
       start: "top center-=100",
     },
   });
-  tl.from(targetElement, {
-    y: "20rem",
+
+  tl.from(".platform--img-animation1", {
+    y: "-20rem",
     opacity: 0,
     duration: 0.3,
     ease: "power1.out",
+  });
+
+  tl.from(".platform--graph-img-6", {
+    opacity: 0,
+    duration: 0.3,
+    ease: "power1.out",
+  });
+
+  tl.from(".platform--graph-img-4", {
+    x: "-20rem",
+    opacity: 0,
+    duration: 0.3,
+    ease: "power1.out",
+  });
+
+  tl.from(".platform--graph-img-5", {
+    x: "20rem",
+    opacity: 0,
+    duration: 0.3,
+    delay: -0.3,
+    ease: "power1.out",
+  });
+
+  tl.from(".platform--graph-img-4-arrow", {
+    x: "-20rem",
+    opacity: 0,
+    duration: 0.3,
+    ease: "power1.out",
+  });
+
+  tl.from(".platform--graph-img-5-arrow", {
+    x: "20rem",
+    opacity: 0,
+    duration: 0.3,
+    delay: -0.3,
+    ease: "power1.out",
+  });
+  let tl2 = gsap.timeline({
+    scrollTrigger: {
+      trigger: triggerElement,
+      start: "top top",
+    },
   });
 });
