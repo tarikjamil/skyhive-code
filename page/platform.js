5d4406d98665fb3e0707acaa318960686ce95280
tarikjamil <tarikjamill@gmail.com>
2023-07-28 11:11:41 +0100

    Update platform.js

---
+++ page/platform.js
@@ -0,0 +1,18 @@
+$(".platform--graph-parent img").each(function (index) {
+  let triggerElement = $(this);
+  let targetElement = $(this);
+
+  let tl = gsap.timeline({
+    scrollTrigger: {
+      trigger: triggerElement,
+      start: "top center-=100",
+    },
+  });
+  tl.from(targetElement, {
+    scale: 0.2,
+    y: "20rem",
+    opacity: 0,
+    duration: 0.6,
+    ease: "power1.out",
+  });
+});
