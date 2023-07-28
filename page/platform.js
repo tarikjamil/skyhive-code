8f0ba4c9c7addb1beb8e191eecac2e7af032090e
tarikjamil <tarikjamill@gmail.com>
2023-07-28 11:26:29 +0100

    Update platform.js

--- page/platform.js
+++ page/platform.js
@@ -11,44 +11,44 @@ $(".platform--graph-parent").each(function (index) {
   tl.from(".platform--img-animation1", {
     y: "-20rem",
     opacity: 0,
-    duration: 0.3,
+    duration: 0.6,
     ease: "power1.out",
     stagger: { each: 0.1, from: "start" },
   });
 
   tl.from(".platform--graph-img-6", {
     opacity: 0,
-    duration: 0.3,
+    duration: 0.6,
     ease: "power1.out",
   });
 
   tl.from(".platform--graph-img-4", {
     x: "-20rem",
     opacity: 0,
-    duration: 0.3,
+    duration: 0.6,
     ease: "power1.out",
   });
 
   tl.from(".platform--graph-img-5", {
     x: "20rem",
     opacity: 0,
-    duration: 0.3,
-    delay: -0.3,
+    duration: 0.6,
+    delay: -0.6,
     ease: "power1.out",
   });
 
   tl.from(".platform--graph-img-4-arrow", {
     x: "-20rem",
     opacity: 0,
-    duration: 0.3,
+    duration: 0.6,
     ease: "power1.out",
   });
 
   tl.from(".platform--graph-img-5-arrow", {
     x: "20rem",
     opacity: 0,
-    duration: 0.3,
-    delay: -0.3,
+    duration: 0.6,
+    delay: -0.6,
     ease: "power1.out",
   });
   let tl2 = gsap.timeline({
