9344b5b8f82cf7948b53167aebcb2e67861366aa
tarikjamil <tarikjamill@gmail.com>
2023-08-31 20:51:35 +0100

    Update civil.js

--- page/civil.js
+++ page/civil.js
@@ -77,3 +77,24 @@ document.addEventListener("DOMContentLoaded", function () {
   });
   splide.mount();
 });
+
+// slider success stories
+document.addEventListener("DOMContentLoaded", function () {
+  let splide = new Splide(".slider2", {
+    type: "slide",
+    perPage: 1,
+    perMove: 1,
+    gap: "48rem",
+    drag: false,
+    arrows: false,
+    pagination: false,
+    breakpoints: {
+      991: {
+        // Tablet
+        gap: "24rem",
+        drag: true,
+      },
+    },
+  });
+  splide.mount();
+});
