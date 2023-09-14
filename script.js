c65852a50910901b02961141a90e1740fdf83723
tarikjamil <tarikjamill@gmail.com>
2023-09-14 15:01:22 +0100

    Update script.js

--- script.js
+++ script.js
@@ -151,3 +151,23 @@ document.addEventListener("DOMContentLoaded", function () {
   });
   splide.mount();
 });
+
+document.addEventListener("DOMContentLoaded", function () {
+  let splide = new Splide(".is--related-ressources", {
+    type: "slide",
+    perPage: 1,
+    perMove: 1,
+    gap: "24rem",
+    drag: false,
+    arrows: false,
+    pagination: false,
+    breakpoints: {
+      991: {
+        // Tablet
+        gap: "24rem",
+        drag: true,
+      },
+    },
+  });
+  splide.mount();
+});
