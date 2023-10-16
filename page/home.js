279faecf273a7b700d1d5b5639e4232954773fb6
tarikjamil <tarikjamill@gmail.com>
2023-10-16 14:57:26 +0100

    Update home.js

--- page/home.js
+++ page/home.js
@@ -50,48 +50,54 @@ $(".home--accordion-trigger").on("click", function () {
 });
 
 document.addEventListener("DOMContentLoaded", function () {
-  function initiateAnimationForContainer(container) {
-    const tags = container.querySelectorAll(".tag--parent");
-    tags[0].classList.add("active"); // Initially, make the first tag active
-    let activeIndex = 1;
-    let cumulativeMovement = 0;
+  function initiateAnimationForContainer(container, delay) {
+    setTimeout(() => {
+      const tags = container.querySelectorAll(".tag--parent");
+      tags[0].classList.add("active"); // Initially, make the first tag active
+      let activeIndex = 1;
+      let cumulativeMovement = 0;
 
-    function animate() {
-      // Remove active class from the previous tag if any
-      if (activeIndex > 0) {
-        tags[activeIndex - 1].classList.remove("active");
-      }
+      function animate() {
+        // Remove active class from the previous tag if any
+        if (activeIndex > 0) {
+          tags[activeIndex - 1].classList.remove("active");
+        }
 
-      // Add active class to the current tag
-      tags[activeIndex].classList.add("active");
+        // Add active class to the current tag
+        tags[activeIndex].classList.add("active");
 
-      cumulativeMovement += tags[activeIndex - 1].getBoundingClientRect().width;
-      container.style.transform = `translateX(-${cumulativeMovement}px)`;
+        cumulativeMovement +=
+          tags[activeIndex - 1].getBoundingClientRect().width;
+        container.style.transform = `translateX(-${cumulativeMovement}px)`;
 
-      activeIndex++;
+        activeIndex++;
 
-      if (activeIndex === tags.length) {
-        setTimeout(() => {
-          container.style.transition = "none"; // Disable transition
-          container.style.transform = "translateX(0)"; // Reset movement
-          tags[tags.length - 1].classList.remove("active"); // Remove active from the last tag
-          tags[0].classList.add("active"); // Make the first tag active
-          activeIndex = 1; // Reset index
-          cumulativeMovement = 0; // Reset cumulative movement
-
+        if (activeIndex === tags.length) {
           setTimeout(() => {
-            container.style.transition = "transform 0.5s"; // Re-enable the transition
-            animate(); // Restart the animation loop from the first tag
-          }, 20);
-        }, 3000); // Match the delay with the transition duration
-      } else {
-        setTimeout(animate, 3000);
-      }
-    }
+            container.style.transition = "none"; // Disable transition
+            container.style.transform = "translateX(0)"; // Reset movement
+            tags[tags.length - 1].classList.remove("active"); // Remove active from the last tag
+            tags[0].classList.add("active"); // Make the first tag active
+            activeIndex = 1; // Reset index
+            cumulativeMovement = 0; // Reset cumulative movement
 
-    setTimeout(animate, 3000);
+            setTimeout(() => {
+              container.style.transition = "transform 0.5s"; // Re-enable the transition
+              animate(); // Restart the animation loop from the first tag
+            }, 20);
+          }, 3000); // Match the delay with the transition duration
+        } else {
+          setTimeout(animate, 3000);
+        }
+      }
+
+      setTimeout(animate, 3000);
+    }, delay);
   }
 
   const containers = document.querySelectorAll(".looping-els");
-  containers.forEach((container) => initiateAnimationForContainer(container));
+  containers.forEach((container, index) => {
+    const delay = index * 1000; // 1 second delay multiplied by the index
+    initiateAnimationForContainer(container, delay);
+  });
 });
