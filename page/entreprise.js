d661ec6c43a7e4a307cdfbdff55ebd815ddc8e18
tarikjamil <tarikjamill@gmail.com>
2023-10-18 11:16:43 +0100

    Update entreprise.js

--- page/entreprise.js
+++ page/entreprise.js
@@ -40,46 +40,48 @@ document.addEventListener("DOMContentLoaded", function () {
   splide.mount();
 });
 
-let imagesAndAnimations = [
-  {
-    selector: ".entreprise--hero-wrapper .img--100.is--entreprise",
-    initial: { opacity: 1, scale: 1 },
-    animation: { opacity: 0, scale: 0.6 },
-  },
-  {
-    selector: ".entreprise--hero-wrapper .entreprise-hero-img1",
-    initial: { x: "150rem", y: "-20rem", opacity: 0, scale: 1.8 },
-    animation: { x: "0rem", y: "0rem", opacity: 1, scale: 1 },
-  },
-  {
-    selector: ".entreprise--hero-wrapper .entreprise-hero-img2",
-    initial: { y: "150rem", opacity: 0, scale: 1.15 },
-    animation: { y: "0rem", opacity: 1, scale: 1 },
-  },
-  {
-    selector: ".entreprise--hero-wrapper .entreprise-hero-img3",
-    initial: { x: "-200rem", opacity: 0, y: "-50rem", scale: 1.5 },
-    animation: { x: "0rem", opacity: 1, y: "0rem", scale: 1 },
-  },
-  {
-    selector: ".entreprise--hero-wrapper .entreprise-hero-img4",
-    initial: { y: "-50rem", opacity: 0, scale: 1.3 },
-    animation: { y: "0rem", opacity: 1, scale: 1 },
-  },
-];
-
-imagesAndAnimations.forEach((item) => {
-  gsap.fromTo(item.selector, item.initial, {
-    ...item.animation,
-    scrollTrigger: {
-      trigger: ".section.is--scroll-fixed",
-      start: "top top", // when the top of the trigger hits the bottom of the viewport
-      end: "bottom bottom", // when the bottom of the trigger hits the top of the viewport
-      scrub: true,
-      markers: false,
+if (window.innerWidth > 998) {
+  let imagesAndAnimations = [
+    {
+      selector: ".entreprise--hero-wrapper .img--100.is--entreprise",
+      initial: { opacity: 1, scale: 1 },
+      animation: { opacity: 0, scale: 0.6 },
     },
+    {
+      selector: ".entreprise--hero-wrapper .entreprise-hero-img1",
+      initial: { x: "150rem", y: "-20rem", opacity: 0, scale: 1.8 },
+      animation: { x: "0rem", y: "0rem", opacity: 1, scale: 1 },
+    },
+    {
+      selector: ".entreprise--hero-wrapper .entreprise-hero-img2",
+      initial: { y: "150rem", opacity: 0, scale: 1.15 },
+      animation: { y: "0rem", opacity: 1, scale: 1 },
+    },
+    {
+      selector: ".entreprise--hero-wrapper .entreprise-hero-img3",
+      initial: { x: "-200rem", opacity: 0, y: "-50rem", scale: 1.5 },
+      animation: { x: "0rem", opacity: 1, y: "0rem", scale: 1 },
+    },
+    {
+      selector: ".entreprise--hero-wrapper .entreprise-hero-img4",
+      initial: { y: "-50rem", opacity: 0, scale: 1.3 },
+      animation: { y: "0rem", opacity: 1, scale: 1 },
+    },
+  ];
+
+  imagesAndAnimations.forEach((item) => {
+    gsap.fromTo(item.selector, item.initial, {
+      ...item.animation,
+      scrollTrigger: {
+        trigger: ".section.is--scroll-fixed",
+        start: "top top", // when the top of the trigger hits the bottom of the viewport
+        end: "bottom bottom", // when the bottom of the trigger hits the top of the viewport
+        scrub: true,
+        markers: false,
+      },
+    });
   });
-});
+}
 
 // PAGE COLOR POWER-UP
 window.addEventListener("DOMContentLoaded", (event) => {
