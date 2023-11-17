497c0e1157032522feac5f65566de172fa6d0e99
tarikjamil <tarikjamill@gmail.com>
2023-11-17 10:06:33 +0100

    Update integration.js

--- page/integration.js
+++ page/integration.js
@@ -2,16 +2,8 @@
 function pageLoad() {
   let tl = gsap.timeline();
 
-  tl.to(".main-wrapper", {
-    opacity: 1,
-    ease: "Quint.easeOut",
-    duration: 0.5,
-  });
-
-  // Add a label to mark the starting point of simultaneous animations
   tl.add("loadingAnimationsStart");
 
-  // Add the 'loading' animation and set its position to the label
   tl.from(
     "[animation=loadingimg]",
     {
@@ -23,12 +15,19 @@ function pageLoad() {
     },
     "loadingAnimationsStart"
   ); // <-- position parameter set to the label
+}
 
-  // Add the 'loading-reverse' animation and set its position to the label
+pageLoad();
+
+// On Page Load
+function pageLoad() {
+  let tl = gsap.timeline();
+
+  tl.add("loadingAnimationsStart");
+
   tl.from(
-    "[animation=loading-reverse]",
+    "[animation=loadingfloor]",
     {
-      y: "-20rem",
       opacity: "0",
       stagger: { each: 0.1, from: "start" },
       ease: "Quint.easeOut",
