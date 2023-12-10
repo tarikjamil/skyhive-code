0cce1ce586fc681e8d45c9c284d9adfa604cdd4c
tarikjamil <tarikjamill@gmail.com>
2023-12-10 13:15:19 +0100

    Update script--navbar.js

--- script--navbar.js
+++ script--navbar.js
@@ -1,42 +1,46 @@
 $(document).ready(function () {
-  // navbar dropdown --------------------- //
-  $(".navbar--dropdown-toggle-new").on("click", function () {
-    // Close other accordions when opening a new one
-    if (!$(this).hasClass("open")) {
-      $(".navbar--dropdown-toggle-new.open").click();
-    }
-
-    let sibling = $(this).siblings(".navbar--dropdown-list-new");
+  function toggleDropdown() {
     let screenWidth = $(window).width();
     let animationDuration = 1;
 
-    if ($(this).hasClass("open")) {
-      // Close the content div if already open
-      if (screenWidth <= 991) {
-        sibling.css("display", "none");
-      } else {
-        sibling.animate({ height: "0px" }, animationDuration);
-      }
+    $(".navbar--dropdown-toggle-new").each(function () {
+      let sibling = $(this).siblings(".navbar--dropdown-list-new");
 
-      $(".navbar--menu--bg-new").hide();
-    } else {
-      // Open the content div if already closed
-      if (screenWidth <= 991) {
-        sibling.css("display", "flex");
+      if ($(this).hasClass("open")) {
+        // Close the content div if already open
+        if (screenWidth <= 991) {
+          sibling.css("display", "none");
+        } else {
+          sibling.animate({ height: "0px" }, animationDuration);
+        }
+
+        $(".navbar--menu--bg-new").hide();
       } else {
-        sibling.css("height", "auto");
-        let autoHeight = sibling.height();
-        sibling.css("height", "0px");
-        sibling.animate({ height: autoHeight }, animationDuration, () => {
+        // Open the content div if already closed
+        if (screenWidth <= 991) {
+          sibling.css("display", "flex");
+        } else {
           sibling.css("height", "auto");
-        });
+          let autoHeight = sibling.height();
+          sibling.css("height", "0px");
+          sibling.animate({ height: autoHeight }, animationDuration, () => {
+            sibling.css("height", "auto");
+          });
+        }
+
+        $(".navbar--menu--bg-new").show();
       }
+    });
+  }
 
-      $(".navbar--menu--bg-new").show();
-    }
-
+  // Handle click event
+  $(".navbar--dropdown-toggle-new").on("click", function () {
     $(this).toggleClass("open");
+    toggleDropdown();
   });
+
+  // Handle window resize event
+  $(window).on("resize", toggleDropdown);
 
   // Handle clicks on .navbar--menu--bg-new
   $(".navbar--menu--bg-new").on("click", function () {
