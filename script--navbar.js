bf357460238a80bb383f603441ecdfef7b1f43c5
tarikjamil <tarikjamill@gmail.com>
2023-12-10 13:10:34 +0100

    Update script--navbar.js

--- script--navbar.js
+++ script--navbar.js
@@ -6,44 +6,40 @@ $(document).ready(function () {
       $(".navbar--dropdown-toggle-new.open").click();
     }
 
-    // Save the sibling of the toggle we clicked on
     let sibling = $(this).siblings(".navbar--dropdown-list-new");
+    let screenWidth = $(window).width();
     let animationDuration = 1;
 
     if ($(this).hasClass("open")) {
       // Close the content div if already open
-      sibling.animate({ height: "0px" }, animationDuration);
+      if (screenWidth <= 991) {
+        sibling.css("display", "none");
+      } else {
+        sibling.animate({ height: "0px" }, animationDuration);
+      }
 
-      // Hide .navbar--menu--bg-new
       $(".navbar--menu--bg-new").hide();
     } else {
       // Open the content div if already closed
-      sibling.css("height", "auto");
-      let autoHeight = sibling.height();
-      sibling.css("height", "0px");
-      sibling.animate({ height: autoHeight }, animationDuration, () => {
+      if (screenWidth <= 991) {
+        sibling.css("display", "flex");
+      } else {
         sibling.css("height", "auto");
+        let autoHeight = sibling.height();
+        sibling.css("height", "0px");
+        sibling.animate({ height: autoHeight }, animationDuration, () => {
+          sibling.css("height", "auto");
+        });
+      }
 
-        // Scroll the page to the accordion, leaving 200 pixels from the top
-        $("html, body").animate(
-          {
-            scrollTop: $(this).offset().top - 200,
-          },
-          animationDuration
-        );
-      });
-
-      // Show .navbar--menu--bg-new
       $(".navbar--menu--bg-new").show();
     }
 
-    // Open and close the toggle div
     $(this).toggleClass("open");
   });
 
   // Handle clicks on .navbar--menu--bg-new
   $(".navbar--menu--bg-new").on("click", function () {
-    // Trigger click on open dropdown toggle
     $(".navbar--dropdown-toggle-new.open").click();
   });
 });
