92e9ea20cec581678ba75fcc5e3a02902ba65e16
tarikjamil <tarikjamill@gmail.com>
2023-12-10 15:09:28 +0100

    Update script--navbar.js

--- script--navbar.js
+++ script--navbar.js
@@ -1,10 +1,10 @@
 $(document).ready(function () {
-  // navbar dropdown --------------------- //
+  // Navbar dropdown logic
   $(".navbar--dropdown-toggle-new").on("click", function () {
-    // Close other accordions and remove class from navbar when opening a new one
+    // Close other accordions when opening a new one
     if (!$(this).hasClass("open")) {
       $(".navbar--dropdown-toggle-new.open").click();
-      $(".navbar").removeClass("dropdown-active"); // Remove class when other dropdown is closed
+      $(".navbar").removeClass("dropdown-active"); // Remove class when another dropdown is closed
     }
 
     let sibling = $(this).siblings(".navbar--dropdown-list-new");
@@ -45,5 +45,19 @@ $(document).ready(function () {
   $(".navbar--menu--bg-new").on("click", function () {
     $(".navbar--dropdown-toggle-new.open").click();
     $(".navbar").removeClass("dropdown-active"); // Remove class when background is clicked
+  });
+
+  // Handle clicks on .navbar--menu-toggle-new for screens below 991px
+  $(".navbar--menu-toggle-new").on("click", function () {
+    let screenWidth = $(window).width();
+
+    if (screenWidth <= 991) {
+      // Toggle 'dropdown-active' class on .navbar
+      if ($(".navbar").hasClass("dropdown-active")) {
+        $(".navbar").removeClass("dropdown-active");
+      } else {
+        $(".navbar").addClass("dropdown-active");
+      }
+    }
   });
 });
