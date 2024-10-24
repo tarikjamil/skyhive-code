907444d2622bd0dd63ba93e45d023a520f218c14
tarikjamil <tarikjamill@gmail.com>
2024-10-24 13:37:42 +0100

    Update script.js

--- script.js
+++ script.js
@@ -97,21 +97,21 @@ window.addEventListener("resize", function () {
 const scrollSpeed = 50;
 function updateScrollingSpeed() {
   document.querySelectorAll(".is--scrolling").forEach((e) => {
-    let t = e.offsetWidth;
-    e.style.setProperty("--scroll-width", `${t}px`),
-      (e.style.animationDuration = `${t / 50}s`);
+    let n = e.offsetWidth;
+    e.style.setProperty("--scroll-width", `${n}px`),
+      (e.style.animationDuration = `${n / 50}s`);
   });
 }
 function updateLanguageIndicator() {
   var e = window.location.hostname,
-    t = document.getElementById("lang");
+    n = document.getElementById("lang");
   e.startsWith("www.")
-    ? (t.innerText = "En")
+    ? (n.innerText = "En")
     : e.startsWith("ja.")
-    ? (t.innerText = "Jp")
+    ? (n.innerText = "Jp")
     : e.startsWith("kr.")
-    ? (t.innerText = "Kr")
-    : (t.innerText = "En");
+    ? (n.innerText = "Kr")
+    : (n.innerText = "En");
 }
 function getSourceValue() {
   return new URLSearchParams(window.location.search).get("source");
@@ -121,80 +121,76 @@ function setSourceFieldValue() {
   e && (document.getElementById("source").value = e);
 }
 function getSourceValue() {
-  let e = new URLSearchParams(window.location.search);
-  return e.get("source");
+  return new URLSearchParams(window.location.search).get("source");
 }
-function appendSourceToLink(e, t) {
-  let n = new URL(e.href);
-  n.searchParams.set("source", t), (e.href = n.toString());
+function appendSourceToLink(e, n) {
+  let t = new URL(e.href);
+  t.searchParams.set("source", n), (e.href = t.toString());
 }
 function appendSourceToLinks() {
   let e = getSourceValue();
-  if (e) {
-    let t = document.querySelectorAll("a");
-    t.forEach((t) => {
-      t.href &&
-        t.hostname === window.location.hostname &&
-        appendSourceToLink(t, e);
+  e &&
+    document.querySelectorAll("a").forEach((n) => {
+      n.href &&
+        n.hostname === window.location.hostname &&
+        appendSourceToLink(n, e);
     });
-  }
 }
 function observeDOM() {
-  let e = new MutationObserver((e) => {
+  new MutationObserver((e) => {
     e.forEach((e) => {
       e.addedNodes.length && appendSourceToLinks();
     });
-  });
-  e.observe(document.body, { childList: !0, subtree: !0 });
+  }).observe(document.body, { childList: !0, subtree: !0 });
 }
 updateScrollingSpeed(),
   window.addEventListener("resize", updateScrollingSpeed),
   document.addEventListener("DOMContentLoaded", function () {
     let e = document.querySelector(".navbar"),
-      t = document.querySelectorAll(".navbar--dropdown-new"),
-      n = null,
+      n = document.querySelectorAll(".navbar--dropdown-new"),
+      t = null,
       o = (e) => {
         if (!e) return;
-        let t = e.querySelector(".navbar--dropdown-list-new"),
-          n = e.querySelector(".navbar--dropdown-icon");
-        gsap.to(t, {
+        let n = e.querySelector(".navbar--dropdown-list-new"),
+          t = e.querySelector(".navbar--dropdown-icon");
+        gsap.to(n, {
           opacity: 0,
           y: "20rem",
           duration: 0.3,
           ease: "smooth",
           onComplete() {
-            t.style.display = "none";
+            n.style.display = "none";
           },
         }),
-          gsap.to(n, { rotation: 0, duration: 0.3, ease: "smooth" });
+          gsap.to(t, { rotation: 0, duration: 0.3, ease: "smooth" });
       },
       a = () => {
-        t.forEach((t) => {
-          t.addEventListener("mouseenter", () => {
+        n.forEach((n) => {
+          n.addEventListener("mouseenter", () => {
             e.classList.add("is--active");
-            let a = t.querySelector(".navbar--dropdown-icon");
-            n && n !== t && o(n), (n = t);
-            let r = t.querySelector(".navbar--dropdown-list-new");
+            let a = n.querySelector(".navbar--dropdown-icon");
+            t && t !== n && o(t), (t = n);
+            let r = n.querySelector(".navbar--dropdown-list-new");
             (r.style.display = "flex"),
               gsap.to(r, { opacity: 1, y: 0, duration: 0.3, ease: "smooth" }),
               gsap.to(a, { rotation: 180, duration: 0.3, ease: "smooth" });
           }),
-            t.addEventListener("mouseleave", (a) => {
+            n.addEventListener("mouseleave", (a) => {
               e.contains(a.relatedTarget) ||
-                (o(t), (n = null), e.classList.remove("is--active"));
+                (o(n), (t = null), e.classList.remove("is--active"));
             });
         }),
           e.addEventListener("mouseleave", () => {
-            n && (o(n), (n = null)), e.classList.remove("is--active");
+            t && (o(t), (t = null)), e.classList.remove("is--active");
           });
       },
       r = () => {
-        t.forEach((e) => {
+        n.forEach((e) => {
           e.removeEventListener("mouseenter", a),
             e.removeEventListener("mouseleave", a);
         }),
           e.removeEventListener("mouseleave", a),
-          n && (o(n), (n = null));
+          t && (o(t), (t = null));
       },
       i = () => {
         window.innerWidth >= 992 ? a() : r();
@@ -208,16 +204,12 @@ updateScrollingSpeed(),
   (window.onload = setSourceFieldValue),
   document.addEventListener("DOMContentLoaded", () => {
     appendSourceToLinks(), observeDOM();
+  }),
+  document.addEventListener("DOMContentLoaded", function () {
+    let e = window.location.hostname,
+      n = ".form--en";
+    "ja.skyhive.ai" === e
+      ? (n = ".form--jp")
+      : "kr.skyhive.ai" === e && (n = ".form--kr"),
+      (document.querySelector(n).style.display = "block");
   });
-document.addEventListener("DOMContentLoaded", function () {
-  const domain = window.location.hostname;
-  let formToDisplay = ".form--en"; // Default form
-
-  if (domain === "ja.skyhive.ai") {
-    formToDisplay = ".form--jp";
-  } else if (domain === "kr.skyhive.ai") {
-    formToDisplay = ".form--kr";
-  }
-
-  document.querySelector(formToDisplay).style.display = "block";
-});
